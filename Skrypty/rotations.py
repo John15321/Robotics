6b34d13cf7dea671061b876308c0411dc52cfd30
John15321 <56229312+John15321@users.noreply.github.com>
2020-11-17 22:15:14 +0100

    .

---
+++ Skrypty/rotations.py
@@ -0,0 +1,31 @@
+"""
+Module contains rotations for robotics.
+"""
+import numpy as numpy
+import matplotlib.pyplot as plt
+import sympy as sp
+from sympy import print_latex
+
+
+def rot_z_fun(alpha):
+    return sp.Matrix([
+        [sp.cos(alpha), -sp.sin(alpha), 0],
+        [sp.sin(alpha),  sp.cos(alpha), 0],
+        [0,              0,             1]
+    ])
+
+
+def rot_x_fun(beta):
+    return sp.Matrix([
+        [1,  0,             0           ],
+        [0,  sp.cos(beta), -sp.sin(beta)],
+        [0,  sp.sin(beta),  sp.cos(beta)]
+    ])
+
+
+def rot_y_fun(gamma):
+    return sp.Matrix([
+        [ sp.cos(gamma), 0, sp.sin(gamma)],
+        [ 0,             1, 0            ],
+        [-sp.sin(gamma), 0, sp.cos(gamma)]
+    ])
